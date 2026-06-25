ab26d4275bd24803758126c1ded418b872a1192d
umoralesroman57-cmyk <umoralesroman57@gmail.com>
2026-06-25 12:05:30 -0600

    Eliminar libro

--- app.py
+++ app.py
@@ -58,6 +58,18 @@ def actualizar_libro():
         print("Error al actualizar un libro")
         print(e)
 
+def eliminar_libro():
+            try:
+                libro_dao = LibroDAO()
+                print("Lista de libros disponibles:")
+                ver_libros()
+                id = int(input("Escribe el id del libro a eliminar: "))
+                libro_dao.eliminar(id)
+                print(f"El libro {id} ha sido eliminado con éxito")
+            except Exception as e:
+                print(f"Error al eliminar el libro {id}")
+                print(e)
+
 
 def main():
     print("=== BIBLIOTECA UNIVERSITARIA ===")
@@ -76,7 +88,7 @@ def main():
         case 3:
             actualizar_libro()
         case 4:
-            #eliminar_libro()
+            eliminar_libro()
 
 
 
